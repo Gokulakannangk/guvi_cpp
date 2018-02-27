4523a3c87ed39244bbb0949b08ebbbf1e588ae63
Gokulakannangk <36393615+Gokulakannangk@users.noreply.github.com>
2018-02-27 18:00:10 +0530

    Check whether a number is positive or negative or zero.

---
+++ set1-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+int num;
+cout<<"enter the num"<<endl;
+cin>>num;
+if(num > 0){
+cout<<"enter the number is positive"<<endl;
+}
+else if(num < 0){
+cout<<"enter the num is negative";
+}
+else if(num == 0){
+cout<<"enter the num is zero";
+}
+return 0;
+}
